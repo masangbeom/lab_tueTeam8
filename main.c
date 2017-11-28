0d91cc31d7bfec88e99f4f1c4a5c6324d444be9a
masangbeom <aktkdqja12@gmail.com>
2017-11-28 22:47:40 +0900

    Bluetooth 셋팅 전.

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_rcc.h"
 #include "stm32f10x_usart.h"
+#include "stm32f10x_tim.h" // timer을 제어하기 위한 함수가 모여잇음
 #include "lcd.h"
 #include "misc.h"
 #include "printFunc.h"
@@ -14,6 +15,8 @@
 char tb_st[7][512] = { {'\0'},{'\0'},{'\0'},{'\0'},{'\0'},{'\0'},{'\0'} };
 
 char showString[2048] = { '\0' };
+
+int new_flag[7];
 
 int bufBLE, tableNUM;
 int sizeBLE[7];
@@ -54,42 +57,45 @@ void UsartInit(void) {
 	USART_Cmd(USART2, ENABLE);
 }
 
-////Occured When detect Rx of USART1
-//void USART1_IRQHandler(void) {
-//	char c;
-//
-//	if (GetPCKey(&c) == 1) { //Get Data Using RX's Signal of USART1
-//		Lb_BT_printf("%c", c); //Send Data Using TX's Signal of USART2
-//	}
-//}
-
 //Occured When detect Rx of USART2
 void USART2_IRQHandler(void) {
     if ((USART2 -> SR & USART_FLAG_RXNE) != (u16) RESET) {
         bufBLE = USART_ReceiveData(USART2);
         if (bufBLE == '@') {
             tableNUM = 1;
+            new_flag[tableNUM] = 1;
             tb_st[tableNUM][0] = '\0';
+            bufBLE = ' ';
         }
         else if (bufBLE == '#') {
             tableNUM = 2;
+            new_flag[tableNUM] = 1;
             tb_st[tableNUM][0] = '\0';
+            bufBLE = ' ';
         }
         else if (bufBLE == '$') {
             tableNUM = 3;
+            new_flag[tableNUM] = 1;
             tb_st[tableNUM][0] = '\0';
+            bufBLE = ' ';
         }
         else if (bufBLE == '%') {
             tableNUM = 4;
+            new_flag[tableNUM] = 1;
             tb_st[tableNUM][0] = '\0';
+            bufBLE = ' ';
         }
         else if (bufBLE == '^') {
             tableNUM = 5;
+            new_flag[tableNUM] = 1;
             tb_st[tableNUM][0] = '\0';
+            bufBLE = ' ';
         }
         else if (bufBLE == '&') {
             tableNUM = 6;
+            new_flag[tableNUM] = 1;
             tb_st[tableNUM][0] = '\0';
+            bufBLE = ' ';
         }
 
         if (sizeBLE[tableNUM] == 512) {
@@ -100,18 +106,45 @@ void USART2_IRQHandler(void) {
         }
         tb_st[tableNUM][sizeBLE[tableNUM]] = '\0';
     }
-
-    /*
-     if(bufBLE != '@'){
-        if (sizeBLE == 2048) {
-                 st[sizeBLE] = bufBLE;
-                 sizeBLE = 0;
-              } else {
-                 st[sizeBLE++] = bufBLE;
-              }
-              st[sizeBLE] = '\0';
-     }*/
 }
+
+void init_Timer() {
+   NVIC_InitTypeDef NVIC_InitStructure;             // for interreupt
+   TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;       // timerbase...
+
+   /* TIM2 Clock Enable */
+   RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+   /* Enable TIM2 Global Interrupt */
+   NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+   NVIC_Init(&NVIC_InitStructure);
+
+   /* TIM2 Initialize */
+   TIM_TimeBaseStructure.TIM_Period = 2000;
+   TIM_TimeBaseStructure.TIM_Prescaler = 35999;
+   //계산방법 : HZ 는 Prescaler가 1이 아니므로 72MHz이다 따라서 1/72MHz * 2000 * 36000 = 1초
+   TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+   TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+   TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+
+   /* TIM2 Enale */
+   TIM_ARRPreloadConfig(TIM2, ENABLE);
+   TIM_Cmd(TIM2, ENABLE);
+   TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE); // interrupt enable
+}
+
+
+//TIM2
+void TIM2_IRQHandler(void) {
+   TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+   //Clears the TIMx's interrupt pending bits.
+}
+
+
+
 int main() {
 	GPIO_InitTypeDef AAA;
 	SystemInit();
@@ -120,21 +153,14 @@ int main() {
 	Touch_Configuration();
 	Touch_Adjust();
 	LCD_Clear(WHITE);
-//	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD,ENABLE);
-//
-//	AAA.GPIO_Pin = GPIO_Pin_2;
-//	AAA.GPIO_Speed = GPIO_Speed_10MHz;
-//	AAA.GPIO_Mode = GPIO_Mode_Out_PP;
-//	GPIO_Init(GPIOD,&AAA);
-
 	while (1) {
 		switch (commandUI) {
 		case WAIT_ORDER: {
-			printWaitUI(&commandUI, &pos_x, &pos_y);
+		    printTableNUM = printWaitUI(&commandUI, &pos_x, &pos_y, new_flag);
 			break;
 		}
 		case NEW_ORDER: {
-			printOrderList(&tb_st[tableNUM], &printTableNUM , &commandUI, &pos_x, &pos_y);
+			printOrderList(tb_st[printTableNUM], &commandUI, &pos_x, &pos_y);
 			break;
 		}
 
--- printFunc.h
+++ printFunc.h
@@ -2,5 +2,5 @@
 #include "misc.h"
 #include "touch.h"
 
-void printOrderList();
-void printWaitUI();
+void printOrderList(char * tb_st, int * commandUI, uint16_t *pos_x, uint16_t *pos_y);
+int printWaitUI(int * commandUI, uint16_t *pos_x, uint16_t *pos_y, int * new_flag);
