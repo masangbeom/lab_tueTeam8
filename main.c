1cd2b6a544dde5b5be371c883048c8a5f55ffe2f
masangbeom <aktkdqja12@gmail.com>
2017-11-28 19:29:22 +0900

    WAIT UI & OrderList 1

--- main.c
+++ main.c
@@ -4,14 +4,19 @@
 #include "stm32f10x_usart.h"
 #include "lcd.h"
 #include "misc.h"
+#include "touch.h"
 #include "Lb_Printf.h"
 #include "Lb_BT_Printf.h"
 #include "stdlib.h"
+#define WAIT_ORDER 0
+#define NEW_ORDER 1
+#define FLAG_BLE_CONNECT '@'
 char st[2048] = {'\0'};
 char showString[2048] = {'\0'};
 int bufBLE, sizeBLE;
 int countStart = 1;
-
+int commandUI = 0;
+uint16_t pos_x, pos_y;
 //Send Data Using TX of USART1
 void print_byte(unsigned short c) {
    while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET)
@@ -137,6 +142,7 @@ void USART2_IRQHandler(void) {
 		if ((USART2->SR & USART_FLAG_RXNE) != (u16)RESET)
 	    {
 	        bufBLE = USART_ReceiveData(USART2);
+
 	         if(countStart != 1){
 	            if(sizeBLE == 512)
 	            {
@@ -151,20 +157,67 @@ void USART2_IRQHandler(void) {
 			}
 	    }
 	countStart = 0;
-//	  char c;
-//
-//	   if (GetBluetoothKey(&c) == 1) { //Get Data Using RX's Signal of USART2
-//	      Lb_printf("%c", c); //Send Data Using TX's Signal of USART1
-//	   }
 }
 
+void printOrderList(){
+	uint16_t pix_x,pix_y;
+	LCD_Clear(WHITE);
+	st = '\0';
+	while(commandUI == 1){
+		LCD_ShowString(10,30,st,BLACK,WHITE);
+		LCD_DrawLine(0, 240, 240, 240);
+		Touch_GetXY(&pos_x, &pos_y, 1);
+		Convert_Pos(pos_x,pos_y,&pix_x,&pix_y);
+		if(pix_x >= 0 && pix_x <= 240 && pix_y >= 240 && pix_y <= 320 ){
+			commandUI = 0;
+		}
+	}
+}
+
+void printWaitUI(){
+	uint16_t pix_x,pix_y;
+	while(commandUI == 0){
+		LCD_DrawLine(0, 103, 240, 103);
+		LCD_DrawLine(0, 206, 240, 206);
+		LCD_DrawLine(120, 0, 120, 320);
+		LCD_ShowString(30, 53, "TABLE 1", BLACK,WHITE);
+		LCD_ShowString(150, 53, "TABLE 2", BLACK,WHITE);
+		LCD_ShowString(30, 153, "TABLE 3", BLACK,WHITE);
+		LCD_ShowString(150, 153, "TABLE 4", BLACK,WHITE);
+		LCD_ShowString(30, 253, "TABLE 5", BLACK,WHITE);
+		LCD_ShowString(150, 253, "TABLE 6", BLACK,WHITE);
+		Touch_GetXY(&pos_x, &pos_y, 1);
+		Convert_Pos(pos_x,pos_y,&pix_x,&pix_y);
+		if(pix_x >= 0 && pix_x <= 120 && pix_y >= 0 && pix_y <= 103) commandUI = 1; // Table 1
+		else if(pix_x >= 120 && pix_x <= 240 && pix_y >= 0 && pix_y <= 103) commandUI = 1; // Table 2
+		else if(pix_x >= 0 && pix_x <= 120 && pix_y >= 103 && pix_y <= 206) commandUI = 1; // Table 3
+		else if(pix_x >= 120 && pix_x <= 240 && pix_y >= 103 && pix_y <= 206) commandUI = 1; // Table 4
+		else if(pix_x >= 0 && pix_x <= 120 && pix_y >= 206 && pix_y <= 320) commandUI = 1; // Table 5
+		else if(pix_x >= 120 && pix_x <= 240 && pix_y >= 206 && pix_y <= 320) commandUI = 1; // Table 6
+	}
+	LCD_Clear(WHITE);
+}
 int main() {
    SystemInit();
    UsartInit();
    LCD_Init();
+   Touch_Configuration();
+   Touch_Adjust();
    LCD_Clear(WHITE);
 
    while (1) {
-	   LCD_ShowString(10,30,st,BLACK,WHITE);
+	   switch(commandUI){
+	   	   case WAIT_ORDER:{
+	   		   printWaitUI(); break;
+	   	   }
+	   	   case NEW_ORDER:{
+	   		   printOrderList(); break;
+	   	   }
+	   	   default: {
+	   		printWaitUI(); break;
+	   	   }
+
+	   }
+
    }
 }
